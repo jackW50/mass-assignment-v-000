e8018db556b13e4599174636963e7a942e2f9967
John Werenski <jtwerenski@gmail.com>
2018-09-30 20:16:02 +0000

    automatically backed up by learn

--- lib/person.rb
+++ lib/person.rb
@@ -1,5 +1,5 @@
 class Person
-  attr_accessor :name, :birthday, :hair_color
+  attr_accessor :name, :birthday, :hair_color, :eye_color, :height
   
   def initialize(attributes)
     attributes.each {|key, value| self.send("#{key}=", value)}
